e440ec84d119de327e8cec824d36fdc72c2eecfc
bgnatowski <bgnatowski00@gmail.com>
2021-10-12 19:31:37 +0200

    add jakies pliki z resta z codegyma

---
+++ rest_example/src/main/java/com/codegym/lectures/rest_example/controller/CustomerController.java
@@ -0,0 +1,62 @@
+package com.codegym.lectures.rest_example.controller;
+
+import com.codegym.lectures.rest_example.model.Customer;
+import com.codegym.lectures.rest_example.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService){
+        this.customerService = customerService;
+    }
+
+    @PostMapping(value = "/customers")
+    public ResponseEntity<?> create(@RequestBody Customer customer){
+        customerService.create(customer);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/customers")
+    public ResponseEntity<List<Customer>> read(){
+        final List<Customer> customers = customerService.readAll();
+
+        return customers != null && !customers.isEmpty()
+                ? new ResponseEntity<>(customers, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping(value = "/customers/{id}")
+    public ResponseEntity<Customer> read(@PathVariable(name = "id") int id){
+        final Customer customer = customerService.read(id);
+
+        return customer != null
+                ? new ResponseEntity<>(customer, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping(value = "/customers/{id}")
+    public ResponseEntity<?> update(@PathVariable(name = "id") int id, @RequestBody Customer customer){
+        final boolean update = customerService.update(customer,id);
+
+        return update
+                ? new ResponseEntity<>(HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+    }
+
+    @DeleteMapping(value = "/customers/{id}")
+    public ResponseEntity<?> delete(@PathVariable(name = "id") int id){
+        final boolean deleted = customerService.delete(id);
+
+        return deleted
+                ? new ResponseEntity<>(HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+    }
+}
---
+++ rest_example/src/main/java/com/codegym/lectures/rest_example/service/CustomerService.java
@@ -0,0 +1,42 @@
+package com.codegym.lectures.rest_example.service;
+
+import com.codegym.lectures.rest_example.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    /**
+     * Creates a new customer
+     * @param customer - Customer to be created
+     */
+    void create(Customer customer);
+
+    /**
+     * Returns a list of all existing customers
+     * @return List of customers
+     */
+    List<Customer> readAll();
+
+    /**
+     * Returns a customer based on its ID
+     * @param id - Customer ID
+     * @return - Customer object with the given ID
+     */
+    Customer read(int id);
+
+    /**
+     * Updates the customer with the given ID,
+     * according to the passed customer
+     * @param customer - Customer to use to update the data
+     * @param id - ID of the customer you want to update
+     * @return - true if the data has been updated, otherwise false
+     */
+    boolean update(Customer customer, int id);
+
+    /**
+     * Deletes the customer with the given ID
+     * @param id - ID of the customer to be deleted
+     * @return - true if the customer was deleted, otherwise false
+     */
+    boolean delete(int id);
+}
