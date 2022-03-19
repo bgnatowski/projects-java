427869e5f5b3ffddeaf0a43974d110b30fc9e6ff
bgnatowski <bgnatowski00@gmail.com>
2022-03-19 19:23:44 +0100

    add empty kalkulatorsredniejagh project

---
+++ KalkulatorSredniejAGH/kalkulator-sredniej-agh/src/main/java/pl/gnatowski/kalkulator/KalkulatorSredniejAghApplication.java
@@ -0,0 +1,17 @@
+package pl.gnatowski.kalkulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalkulatorSredniejAghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalkulatorSredniejAghApplication.class, args);
+
+
+		shot
+
+	}
+
+}
---
+++ KalkulatorSredniejAGH/kalkulator-sredniej-agh/src/test/java/pl/gnatowski/kalkulator/KalkulatorSredniejAghApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.gnatowski.kalkulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KalkulatorSredniejAghApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
